989afc363a4229afd0e923babe9a49d61baf46ca
Sohal Singh <singhsohal575@gmail.com>
2023-06-19 13:21:34 +0530

    first commit

---
+++ zodiacsignfinder.py
@@ -0,0 +1,38 @@
+def get_zodiac_sign(day, month):
+    if (month == 1 and day >= 20) or (month == 2 and day <= 18):
+        return "Aquarius"
+    elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+        return "Pisces"
+    elif (month == 3 and day >= 21) or (month == 4 and day <= 19):
+        return "Aries"
+    elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
+        return "Taurus"
+    elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
+        return "Gemini"
+    elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
+        return "Cancer"
+    elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
+        return "Leo"
+    elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
+        return "Virgo"
+    elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
+        return "Libra"
+    elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
+        return "Scorpio"
+    elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
+        return "Sagittarius"
+    else:
+        return "Capricorn"
+
+def main():
+    try:
+        day = int(input("Enter the day of your birth: "))
+        month = int(input("Enter the month of your birth: "))
+
+        zodiac_sign = get_zodiac_sign(day, month)
+        print("Your zodiac sign is", zodiac_sign)
+    except ValueError:
+        print("Invalid input. Please enter numeric values for day and month.")
+
+if __name__ == "__main__":
+    main()
